a5721a742ede319f9801ad718e1aa3f5f377fed9
Carey Lai <chunlcc65@gmail.com>
2026-05-05 21:34:02 +0800

    Fill in missing arguments for Streamlit widgets

--- In_Class_Exercise_5_MoreInfo.py
+++ In_Class_Exercise_5_MoreInfo.py
@@ -20,7 +20,7 @@ with st.sidebar:
     # Checkbox widget
     # fill in the code below
     # the first argument should be the label (a str), the second argument should be the default value
-    show_sector = st.checkbox(____, value=____)
+    show_sector = st.checkbox("Show Market Cap by Sector", value=True)
 
     # Multiselect widget
     # fill in the code below
@@ -28,9 +28,9 @@ with st.sidebar:
     # the second argument should be the S&P100 tickers, 
     # the third argument should be the default options
     selected_tickers = st.multiselect(
-        ____,
-        options=____,
-        default=____
+        "Select Stock Tickers",
+        options=tickers_100,
+        default=['TSLA', 'NVDA', 'AAPL']
     )
 
     # Year range slider
@@ -39,28 +39,28 @@ with st.sidebar:
     # the second (third) argument should be the min (max) value, 
     # the last argument should be the default value range
     selected_years = st.slider(
-        ____,
-        min_value=____,
-        max_value=____,
-        value=(____, ____)
+        "Select Year Range",
+        min_value=min_year,
+        max_value=max_year,
+        value=(min_year, max_year)
     )
 
 # a bar chart that visualizes the market cap of S&P 100 stocks by sector
 # fill in the code below
 if show_sector:
-    st.header("____")
+    st.header("Market Capitalization by Sector")
     # the first argument is a dataframe
     # the second, third, and fourth arguments are some columns in the dataframe
     # the fifth argument specifies whether you want the bar chart to be horizontal.
 
-    st.bar_chart(____, x=____, y=____, color=____, horizontal=____,
+    st.bar_chart(ticker_info, x="Sector", y="Market Cap (B)", color="Sector", horizontal=True,
             width=720, height=500)
 
 # display price and volme charts if stocks are selected; show error message otherwise
 # fill in the code below
 if selected_tickers:
     # show the header with using f-string
-    st.header(f"____")
+    st.header(f"Prices & Volume for Selected Stocks")
     # select the required data
     chart_data = stock_data.query(f"Date < {selected_years[1] + 1} and Date >= {selected_years[0]} and Ticker in {selected_tickers}")
  
@@ -68,20 +68,20 @@ if selected_tickers:
     st.subheader("Closing Prices")
     # the first argument is a dataframe
     # the second, third, and fourth arguments are some columns in the dataframe
-    st.line_chart(____, x=____, y=____, color="Ticker",
+    st.line_chart(chart_data, x="Date", y="Close", color="Ticker",
                   width=720, height=500)
 
     # Bar chart for volume
     st.subheader("Trading Volume")
     # the first argument is a dataframe
     # the second, third, and fourth arguments are some columns in the dataframe
-    st.bar_chart(____, x=____, y=____, color=____, 
+    st.bar_chart(chart_data, x="Date", y="Volume", color="Ticker", 
                   width=720, height=500)
 
 # display the error message
 # fill in the code below
 else:
-    st.error(____)
+    st.error("Please select at least one stock!")
 
 
 
